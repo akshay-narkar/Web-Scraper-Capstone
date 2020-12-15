9118b86bb2cc84fdd095e858a0ec5ba33d5b5fd0
akshay-narkar <akshaynarkar1295@gmail.com>
2020-12-15 19:02:28 +0530

    Remove all puts. Add single object

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 require 'byebug'
+require 'csv'
 require 'nokogiri'
 require 'open-uri'
 require 'watir'
@@ -8,32 +9,37 @@ require_relative '../lib/scraper'
 
 play_again = 'Y'
 
-def new_player
-  puts 'Insert player id'
-  id = gets.chomp.strip
-  check = false
-  valid_obj = Valid.new
-  until check == true
-    check = valid_obj.validate(id)
-    next unless check == false
+# def new_player
+#   puts 'Insert player id'
+#   id = gets.chomp.strip
+#   check = false
+#   valid_obj = Valid.new
+#   until check == true
+#     check = valid_obj.validate(id)
+#     next unless check == false
 
-    p check
-    puts 'Your Id is invalid', ''
-    puts
-    puts 'Enter correct Id', ''
-    id = gets.chomp.strip
-  end
-  puts "You entered the id  #{id}"
-  id
-end
+#     p check
+#     puts 'Your Id is invalid', ''
+#     puts
+#     puts 'Enter correct Id', ''
+#     id = gets.chomp.strip
+#   end
+#   puts "You entered the id  #{id}"
+#   id
+# end
 
-while %w[y Y].include?(play_again)
-  id = new_player
-  scrape = Scraper.new(id)
-  data = scrape.scraper_tool
-  puts 'Would you like to check more ids? Press Y to continue, anything else to quit', ''
-  play_again = gets.chomp.strip
-end
+top100 = Top.new
+top100.top100header
+top100.top100
+#  top100.csv
+
+# while %w[y Y].include?(play_again)
+#   id = new_player
+#   scrape = Scraper.new(id)
+#   data = scrape.scraper_tool
+#   puts 'Would you like to check more ids? Press Y to continue, anything else to quit', ''
+#   play_again = gets.chomp.strip
+# end
 
 # root > div:nth-child(2) > div > div.Layout__Main-eg6k6r-1.haICgV > div:nth-child(2) >
 # div.Table__ScrollTable-ziussd-0.canFyp > table > tbody
