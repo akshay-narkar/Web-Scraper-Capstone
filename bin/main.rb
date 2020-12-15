3b9fbebbb5202b5007a83628a251f226aa90389c
akshay-narkar <akshaynarkar1295@gmail.com>
2020-12-15 21:52:38 +0530

    Add class and methods to main

--- bin/main.rb
+++ bin/main.rb
@@ -7,7 +7,7 @@ require 'webdrivers'
 require_relative '../lib/validation'
 require_relative '../lib/scraper'
 
-play_again = 'Y'
+# play_again = 'Y'
 
 # def new_player
 #   puts 'Insert player id'
@@ -28,9 +28,40 @@ play_again = 'Y'
 #   id
 # end
 
-top100 = Top.new
-top100.top100header
-top100.top100
+class Intro
+  def scraper_intro
+    puts 'Welcome to The FPL Data Scraper'
+    puts 'Here you can get the data of Top FPL Players into your CSV file'
+    puts 'Insert a number in multiples of 50 to scrape data from'
+    puts 'If you input 50, top 50 players data with link would be available and so on'
+  end
+
+  def take_input
+    number = gets.chomp.strip.to_i
+    until (number % 50).zero? && number != 0
+      puts 'Invalid Number, please enter valid number'
+      number = gets.chomp.strip
+    end
+    @newnumber = (number / 50) - 1
+  end
+
+  def check_page
+    top100 = Top.new
+    top100.top100noko
+    top100.header
+    top100.top100
+
+    while @newnumber.positive?
+      top100.pagination100
+      @newnumber -= 1
+    end
+  end
+end
+
+intro = Intro.new
+intro.scraper_intro
+intro.take_input
+intro.check_page
 #  top100.csv
 
 # while %w[y Y].include?(play_again)
